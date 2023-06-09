0ce002341dbaf5476dc1c1557bd8c56ca5cfaeb5
Jay Chulani <jay.chulani@me.com>
2023-06-08 23:14:31 -0700

    Make use of Pie class; draw using weird angles

--- memorize/EmojiMemoryGameView.swift
+++ memorize/EmojiMemoryGameView.swift
@@ -57,7 +57,9 @@ struct CardView: View {
                 if card.isFaceUp {
                     shape.fill().foregroundColor(.white)
                     shape.strokeBorder( lineWidth: DrawingConstants.lineWidth )
-                    Circle().padding(5).opacity(0.5)
+                    Pie(startAngle: Angle(degrees: 270), endAngle: Angle(degrees: 45), clockwise: true)
+                        .padding(5)
+                        .opacity(0.5)
                     Text(card.content).font(font(in: geometry.size))
                 } else if card.isMatched{
                     
