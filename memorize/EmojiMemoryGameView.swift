9bb80f72f5740f23f8078f84aad4f395562cc205
Jay Chulani <jay.chulani@me.com>
2023-08-07 22:03:45 -0700

    animated pie based on bonusTimeRemaining

--- memorize/EmojiMemoryGameView.swift
+++ memorize/EmojiMemoryGameView.swift
@@ -129,12 +129,25 @@ struct CardView: View {
     // This CardView takes a Card and builds a body from it
     let card: EmojiMemoryGame.Card
     
+    @State private var animatedBonusRemaining:Double = 0
     var body: some View {
         GeometryReader(content: { geometry in
             ZStack {
-                Pie(startAngle: Angle(degrees: 270), endAngle: Angle(degrees: 45), clockwise: true)
-                    .padding(5)
-                    .opacity(0.5)
+                Group {
+                    if card.isConsumingBonusTime {
+                        Pie(startAngle: Angle(degrees: 0-90), endAngle: Angle(degrees: (1-animatedBonusRemaining)*360-90))
+                            .onAppear {
+                                animatedBonusRemaining = card.bonusRemaining
+                                withAnimation(.linear(duration: card.bonusTimeRemaining)) {
+                                    animatedBonusRemaining = 0
+                                }
+                            }
+                    } else {
+                        Pie(startAngle: Angle(degrees: 0-90), endAngle: Angle(degrees: (1-card.bonusRemaining)*360-90))
+                    }
+                }
+                .padding(5)
+                .opacity(0.5)
                 Text(card.content)
                     .rotationEffect(Angle.degrees(card.isMatched ? 360 : 0))
                     .animation(Animation.linear(duration: 1).repeatForever(autoreverses: false))
